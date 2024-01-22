84a1081ab00c871fd48033f77ecaf652929e297f
Vasilij Moiseev <vmoiseev@express42.com>
2024-01-23 00:29:57 +0300

    7. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
+    @Override
     public String generate(String name, String body) {
         String json = "{%n\t\"name\" : \"%s\",%n\t\"body\" : \"%s\"%n}";
         return json.formatted(name, body);
