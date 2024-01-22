0579707373ec306c5b5d99aba91eabe04052d9d6
Vasilij Moiseev <vmoiseev@express42.com>
2024-01-22 22:01:46 +0300

    3. Вызов конструктора родителя super(...) [#6837]

---
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class Engineer extends Profession {
+
+    private int experience;
+
+    public Engineer(int experience, boolean degree) {
+        super(degree);
+        this.experience = experience;
+    }
+}
