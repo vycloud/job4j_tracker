50bab166041b1407f9cace8c527e3427fc0c8874
Vasilij Moiseev <vmoiseev@express42.com>
2024-01-27 12:01:28 +0300

    3. Массивы и модели. [#174892 #476863]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -22,7 +22,7 @@ public class Library {
         }
         System.out.println();
         for (Book book : books) {
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + ", " + book.getPages() + " pages");
             }
         }
