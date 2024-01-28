24e46777c28c85dd6f67bbff18ffeddca6d0c28e
Vasilij Moiseev <vmoiseev@express42.com>
2024-01-29 00:04:49 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,24 +47,25 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-        boolean result = false;
         int index = indexOf(id);
-        if (index != -1) {
+        boolean result = index != -1;
+        if (result) {
             item.setId(id);
             items[index] = item;
-            result = true;
         }
         return result;
     }
 
-    public void delete(int id) {
+    public boolean delete(int id) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean result = index != -1;
+        if (result) {
             int startPos = index + 1;
             int length = size - index - 1;
             System.arraycopy(items, startPos, items, index, length);
             items[size - 1] = null;
             size--;
         }
+        return result;
     }
 }
