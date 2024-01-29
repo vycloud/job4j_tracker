ca1393fcd33d8bbc911715698a2d8bea377dbdde
Vasilij Moiseev <vmoiseev@express42.com>
2024-01-29 22:40:18 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,5 +10,6 @@ public class StartUI {
         LocalDateTime created = item.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(created.format(formatter));
+        System.out.println(item);
     }
 }
